8a0b7e6428122c911a9f849d9cc5c245feba17a1
Reza-ashrafi <115076971+Reza-ashrafi@users.noreply.github.com>
2026-07-06 03:55:40 +0330

    Update main.py

--- main.py
+++ main.py
@@ -1,33 +1,4 @@
-import time
-from telegram_sender import send_report
+import os
 
-
-# =========================
-# اجرای یک‌باره گزارش
-# =========================
-def run_once():
-    send_report()
-
-
-# =========================
-# اجرای روزانه (لوکال / سرور)
-# =========================
-def run_daily():
-    while True:
-        try:
-            print("📊 Running daily silver report...")
-            send_report()
-            print("✅ Report sent successfully")
-
-        except Exception as e:
-            print("❌ Error:", e)
-
-        # 24 ساعت صبر
-        time.sleep(86400)
-
-
-# =========================
-# حالت اجرا
-# =========================
-if __name__ == "__main__":
-    run_daily()
+print("BOT_TOKEN =", os.getenv("BOT_TOKEN"))
+print("CHAT_ID =", os.getenv("CHAT_ID"))
